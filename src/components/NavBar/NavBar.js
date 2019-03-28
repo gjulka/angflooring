ed78bd3804a91df067c143a835e09bcae57785c6
gjulka <g.julka10@gmail.com>
2019-03-28 15:52:36 -0500

    additions to navbar/more details/homepage

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,23 +1,27 @@
 import React from 'react';
+import classes from './NavBar.module.css';
 
 
 const Navbar = () => (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="navbar-brand">Navbar</div>
-        <div className="collapse navbar-collapse">
-            <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Products</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    <div className={classes.NavBar}>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="navbar-brand">ANG Flooring</div>
+            <div className="collapse navbar-collapse moveRight">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    
 )
 
 export default Navbar;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -3,17 +3,21 @@ import homepic from '../../assets/homepic.jpg'
 import classes from './Home.module.css';
 
 const Home = () => (
-    <div className={classes.Home}>
-        <div className={classes.HomeBody}>
-            <h1>Choose your next floor</h1>
-            <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
-                in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </h6>
+    <div>
+        <div className={classes.Home}>
+            <div className={classes.HomeBody}>
+                <h1>Choose your next floor</h1>
+                <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+                    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                    sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </h6>
+            </div>
         </div>
-            
+        <div className={classes.Details}>
+            <h1>more details</h1>
+        </div>
     </div>
 )
 
